9526989e65bad4dd0c7d65ef1edb6ef4759ad48d
rajnikant <rajni@welfareinfotech.com>
2020-05-01 18:51:16 +0530

    updated settings

--- TaskPrj/settings.py
+++ TaskPrj/settings.py
@@ -1,4 +1,4 @@
-import django-heroku
+import django_heroku
 import os
 import environ
 
@@ -112,6 +112,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
+)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
@@ -120,4 +122,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "task"
 LOGIN_URL = "login"
 
-django_heroku
+django_heroku.settings(locals())
